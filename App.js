f6279ec06d48c8a408bb9ab119e415d38366286f
Bashir Mohamed Ali <takursiin@gmail.com>
2021-03-03 09:23:03 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+import CameraPage from "./src/camera.page";
+const App = () => {
+  return <CameraPage />;
+};
+
+export default App;
