8ce08e51b4f30894776415eeeec40efa47b80514
BashirMohamedAli <takursiin253@gmail.com>
2024-07-23 17:03:08 +0300

    v2 changed public

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text } from "react-native";
-import CameraPage from "./src/camera.page";
-const App = () => {
-  return <CameraPage />;
-};
-
-export default App;
